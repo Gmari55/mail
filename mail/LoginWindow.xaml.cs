fe0e59d84cc7df893f62834e9f503afa5d171a63
Gmari <potapchuk.maks@gmail.com>
2023-01-09 03:29:47 +0200

    Add project files.

---
+++ mail/LoginWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using System;
+using System.Windows;
+
+namespace mail
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginBtnClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var client = new SmtpClient();
+                client.Connect("smtp.gmail.com", 465, SecureSocketOptions.SslOnConnect);
+                client.Authenticate(EmailTxtBox.Text, PasswordTxtBox.Text);
+                User user = new User();
+                user.Email = EmailTxtBox.Text;
+                user.Password = PasswordTxtBox.Text;
+                MainWindow window = new MainWindow(user);
+                window.Show();
+                this.Close();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("uncorect login or password");
+                throw;
+            }
+
+        }
+
+       
+    }
+}
---
+++ mail/SendWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Win32;
+using System.Net;
+using System.Net.Mail;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace mail
+{
+    public partial class SendWindow : Window
+    {
+
+        User user = new User();
+        MailMessage Mail = new MailMessage();
+        public SendWindow(User user)
+        {
+            InitializeComponent();
+            this.user = user;
+
+        }
+
+        private void SendBtnClick(object sender, RoutedEventArgs e)
+        {
+            MailAddress mailAddress = new MailAddress(user.Email);
+            Mail.From = mailAddress;
+            Mail.To.Add(toTxtBox.Text);
+            Mail.Subject = subjectTxtBox.Text;
+            Mail.Body = bodyTxtBox.Text;
+
+
+
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587)
+            {
+                Credentials = new NetworkCredential(user.Email, user.Password),
+                EnableSsl = true
+            };
+
+            client.Send(Mail);
+            this.Close();
+        }
+
+
+
+        private void AttachBtnClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == true)
+                Mail.Attachments.Add(new Attachment(dialog.FileName));
+        }
+
+
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton li = (sender as RadioButton);
+            switch (li.Content.ToString())
+            {
+                case "Low":
+                    Mail.Priority = MailPriority.Low;
+                    break;
+                case "Normal":
+                    Mail.Priority = MailPriority.Normal;
+                    break;
+                case "High":
+                    Mail.Priority = MailPriority.High;
+                    break;
+            }
+
+
+        }
+    }
+}
